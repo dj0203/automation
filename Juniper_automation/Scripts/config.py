f8f2975a90b0aa70f6e7ab0d63f2ad260f17739b
dj0203 <debjyotipaul68@gmail.com>
2026-02-25 04:19:32 +0530

    Added protocols

--- Juniper_automation/Scripts/config.py
+++ Juniper_automation/Scripts/config.py
@@ -2,24 +2,40 @@ from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from dotenv import load_dotenv
 import os
+import yaml
 
 load_dotenv()
 username = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
-devices = {
-    "172.20.20.2" : {"intf":"eth1"},
-    "172.20.20.3" : {"intf":"eth1"},
-}
 lo_ip = "1.1.1"
 iso_ip ="49.0001.0000.0000"
 
-for count , (ip,params) in enumerate(devices.items(),start =1):
+def connect(ip,username,password):
+    return Device(host=ip,user=username,passwd=password)
+
+with open("/home/deb/projects/automation/Juniper_automation/inventory.yaml") as f:
+    inventory = yaml.safe_load(f)
+
+for count , device in enumerate(inventory["juniper_devices"],start =1):
+    name = device["name"]
+    ip = device ["ip"]
+    role = device["role"]
     lo_addr = f"{lo_ip}.{count}/32"
     iso_addr = f"{iso_ip}.000{count}.00"
-    params["lo_ip"] = lo_addr
-    params["iso_ip"] = iso_addr 
-    print(f"Applying ISO & MPLS configuration on Devices with {ip} | Loopback address {params['lo_ip']} and ISO configurartion {params['iso_ip']} ")
-    with Device (host = ip , user = username , passwd = password) as dev: 
+    print(f"\n{name} → loopback {lo_addr} role {role}")
+    
+    with connect(ip, username, password) as dev:
         cu = Config(dev)
-        cu.load(template_path="config.j2" , template_vars=params, format = "set")
+        cu.load(template_path="/home/deb/projects/automation/Juniper_automation/Templates/loopback.j2",template_vars={"lo_ip": lo_addr},format="set")
         cu.commit()
+        print(f"  {name} loopback configured")
+        
+        if role != "ce":
+            for intf in device["interfaces"]:
+                with connect(ip, username, password) as dev:
+                    cu = Config(dev)
+                    cu.load(template_path="/home/deb/projects/automation/Juniper_automation/Templates/protocols.j2",
+                        template_vars={"iso_ip": iso_addr, "intf": intf},
+                        format="set")
+                    cu.commit()
+                    print(f"  {name} {intf} ISP protocols configured")
